26cda42760fb69f59eaf2b594a1b2167cb076189
DanielSterzel <daniel@sterzel>
2025-10-27 18:10:15 +0100

    some function implementations

--- cpp_sim/OrderBook.cpp
+++ cpp_sim/OrderBook.cpp
@@ -1,1 +1,29 @@
 #include "OrderBook.h"
+
+
+void OrderBook::addOrder(const Order &order) {
+    if (order.isBid) {
+        auto& queue = bids[order.price];
+        queue.emplace_back(order);
+    }else {
+        auto& queue = asks[order.price];
+        queue.emplace_back(order);
+    }
+}
+
+std::optional<std::pair<double, double>> OrderBook::bestPrices() const{
+
+    //check is trade expired?
+
+    if (bids.empty() or asks.empty()) {
+        return std::nullopt;
+    }
+    auto bestBidQueue = bids.begin()->second;
+    auto bestAskQueue = asks.begin()->second;
+
+    if (bestBidQueue.empty() or bestAskQueue.empty()) {
+        return std::nullopt;
+    }
+
+    return std::make_pair(bestBidQueue.front().price, bestAskQueue.front().price);
+}
